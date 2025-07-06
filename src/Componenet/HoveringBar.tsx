d8f2f0e14dff5d4a40670052de4249022317765a
Adetia <adityaxsetia@gmail.com>
2025-07-07 00:42:46 +0530

    HoveringBar.tsx

--- src/Componenet/HoveringBar.tsx
+++ src/Componenet/HoveringBar.tsx
@@ -13,9 +13,9 @@ const HoveringBar = () => {
     <div className="w-80 h-16 fixed backdrop-blur-xs z-50 shadow flex gap-5 justify-between border-2 border-slate-800 p-7 items-center mt-5 rounded-4xl ">
       <div className="flex flex-erap justify-evenly items-center w-full ">
         <NavLink to="/">
-         <i className="fa fa-home text-white text-2xl rounded-4xl cursor-pointer hover:text-black hover:backdrop-blur-sm hover:bg-slate-600 p-3 duration-200 hover:scale-125" onClick={handleClickhome}></i></NavLink>
-         <NavLink to="/projects"><i className="fa fa-file text-white text-2xl  rounded-4xl cursor-pointer hover:text-black hover:bg-slate-500 p-3 hover:backdrop-blur-sm duration-200 hover:scale-125"></i></NavLink>
-         <NavLink to="/contact" ><i className="fa fa-envelope text-white text-2xl  rounded-4xl cursor-pointer hover:text-black hover:bg-slate-600 p-3 duration-200 hover:scale-125 " ></i></NavLink>
+         <i className="fa fa-home text-white text-2xl rounded-4xl cursor-pointer hover:text-black hover:backdrop-blur-sm hover:bg-white p-3 duration-200 hover:scale-125" onClick={handleClickhome}></i></NavLink>
+         <NavLink to="/projects"><i className="fa fa-file text-white text-2xl  rounded-4xl cursor-pointer hover:text-black hover:bg-white p-3 hover:backdrop-blur-sm duration-200 hover:scale-125"></i></NavLink>
+         <NavLink to="/contact" ><i className="fa fa-envelope text-white text-2xl  rounded-4xl cursor-pointer hover:text-black hover:bg-white p-3 duration-200 hover:scale-125 " ></i></NavLink>
          
       </div>
     </div>
