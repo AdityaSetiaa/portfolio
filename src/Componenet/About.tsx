d622a14f63ca8b829d1f8f99db71b3b3264bc7aa
Adetia <adityaxsetia@gmail.com>
2025-09-11 11:27:09 +0530

    About.tsx

--- src/Componenet/About.tsx
+++ src/Componenet/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
         <p className="text-8xl text-white p-2">Hello,</p>
         
        <div>
-       <p className="text-white font-extralight  pl-4">I'm Aditya, a BCA graduate building front-end interfaces via TypeScript, React and Tailwind Css</p>
+       <p className="text-white font-extralight  pl-4">I'm Aditya. I love transforming ideas into sleek, responsive front-end experiences that are both beautiful and functional. I specialize in TypeScript, React, and Tailwind CSS, building interfaces that are intuitive, performant, and a joy to use. Every project for me is an opportunity to craft seamless user experiences while experimenting with modern design and interaction patterns.</p>
         
         <p className="text-white font-extralight pl-4">
           i like to learn new stuff and post about it on my <a href="https://x.com/Adetia108" className="text-cyan-500">Twitter</a>.</p>
